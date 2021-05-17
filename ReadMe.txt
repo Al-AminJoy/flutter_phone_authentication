a25a6ba2c8d5db52159063a9bb8c4109981243c6
Al-AminJoy <alaminislam3555@gmail.com>
2021-05-17 18:17:26 +0600

    Firebase Phone Authentication

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Totorial Followed By-
+EasyApproach(Youtube)- https://www.youtube.com/watch?v=W19IfZ-nqB8&t=95s
---
+++ flutter_phone_authentication/android/app/src/main/kotlin/com/alamin/flutter_phone_authentication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alamin.flutter_phone_authentication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
